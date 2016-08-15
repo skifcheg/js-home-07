eb1df36f38c3e1b6d860a2c2f99a75aa77f7cafb
Sasha <skif@ki-technology.ru>
2016-08-16 02:51:48 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@
     $.fn.trunc = function(options) {
         'use strict';  
         
-        var settings = $.extend({
-            count: 200,
-            afterText: "..."
-        }, options);
-        
-        var thisText = this[0].innerText;
-        
-        if(thisText.length < settings.count){
-           
-        } 
-        
+        var count = options || 200;
+        var shortText = "";
+        console.log(count);
+       
+        for( var i = 0; i < this.length; i++) {
+        	console.log(this);
+		  	var thisText = this[i].innerText; 
+		  	(thisText.length >= count) ? shortText = thisText.substring(0, count) + "..." : shortText = thisText;
+        	this.eq(i).html("<span style='background: yellow;cursor: pointer' title='"+ thisText +"'>"+ shortText +"</span>");
+		}
+
         return this;
     }; 
 }(jQuery));
