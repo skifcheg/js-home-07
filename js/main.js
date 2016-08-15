8294b05b42fca3c213665f18251a2ffaad0ba055
Sasha <skif@ki-technology.ru>
2016-08-16 02:53:19 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -4,10 +4,8 @@
         
         var count = options || 200;
         var shortText = "";
-        console.log(count);
        
         for( var i = 0; i < this.length; i++) {
-        	console.log(this);
 		  	var thisText = this[i].innerText; 
 		  	(thisText.length >= count) ? shortText = thisText.substring(0, count) + "..." : shortText = thisText;
         	this.eq(i).html("<span style='background: yellow;cursor: pointer' title='"+ thisText +"'>"+ shortText +"</span>");
