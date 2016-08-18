479e071c7c3c2e4573f0889d215efbb7d8770d17
Sasha <skif@ki-technology.ru>
2016-08-19 01:21:02 +0300

    add

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
         for( var i = 0; i < this.length; i++) {
 		  	var thisText = this[i].innerText; 
 		  	(thisText.length >= count) ? shortText = thisText.substring(0, count) + "..." : shortText = thisText;
-        	this.eq(i).html("<span style='background: yellow;cursor: pointer' title='"+ thisText +"'>"+ shortText +"</span>");
+        	this.eq(i).html("<span class='wrap-text' title='"+ thisText +"'>"+ shortText +"</span>");
 		}
 
         return this;
